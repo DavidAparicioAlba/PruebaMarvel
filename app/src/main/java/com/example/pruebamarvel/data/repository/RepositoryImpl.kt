787681f0f6e1dc5f4ebd3201b9e01f5135b35b9f
David Aparicio <apadavid@gmail.com>
2024-07-05 01:24:16 +0200

    add: RepositoryImpl constructor added

--- app/src/main/java/com/example/pruebamarvel/data/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/pruebamarvel/data/repository/RepositoryImpl.kt
@@ -1,4 +1,14 @@
-package com.example.pruebamarvel
+package com.example.pruebamarvel.data.repository
 
-class RepositoryImpl {
+import com.example.pruebamarvel.data.remote.ApiService
+import com.example.pruebamarvel.domain.model.DataModel
+import com.example.pruebamarvel.domain.repository.Repository
+import javax.inject.Inject
+
+class RepositoryImpl @Inject constructor(
+    private val apiService: ApiService
+) : Repository {
+    override suspend fun getData(): List<DataModel> {
+        return apiService.fetchData()
+    }
 }
