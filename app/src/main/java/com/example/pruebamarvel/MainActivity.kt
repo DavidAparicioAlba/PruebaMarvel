a0a2b0dbd9fa04a12d5494aa2ea1c6976b5943a0
David Aparicio <apadavid@gmail.com>
2024-07-05 01:25:57 +0200

    update: MainActivity content modified

--- app/src/main/java/com/example/pruebamarvel/MainActivity.kt
+++ app/src/main/java/com/example/pruebamarvel/MainActivity.kt
@@ -11,21 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pruebamarvel.presentation.view.MainScreen
 import com.example.pruebamarvel.ui.theme.PruebaMarvelTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PruebaMarvelTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
     }
 }
