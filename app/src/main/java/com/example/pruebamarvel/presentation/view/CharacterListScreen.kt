2bfe6fd740c6e1f38135a3a2e93d2135c4a6444b
David Aparicio <apadavid@gmail.com>
2024-07-05 17:48:09 +0200

    update: CharacterListScreen refactor

---
+++ app/src/main/java/com/example/pruebamarvel/presentation/view/CharacterListScreen.kt
@@ -0,0 +1,76 @@
+package com.example.pruebamarvel.presentation.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import com.example.pruebamarvel.R
+import com.example.pruebamarvel.domain.model.HeroModel
+
+enum class SortOption { NAME, ID }
+
+@Composable
+fun CharacterList(data: List<HeroModel>, modifier: Modifier) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(data) { character ->
+            CharacterItem(character)
+        }
+    }
+}
+
+@Composable
+fun CharacterItem(character: HeroModel) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { /* Navigate to detail screen */ }
+    ) {
+        val imageUrl = character.thumbnail.fullPath
+        Image(
+            painter = rememberAsyncImagePainter(
+                ImageRequest.Builder(LocalContext.current).data(data = imageUrl).apply(block = fun ImageRequest.Builder.() {
+                    crossfade(true)
+                    error(R.drawable.ic_error_foreground) // Ensure you have a placeholder or error drawable
+                }).build()
+            ),
+            contentDescription = null,
+            modifier = Modifier.size(64.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            Text(text = character.name, style = MaterialTheme.typography.titleMedium)
+            Text(text = character.description, style = MaterialTheme.typography.bodyMedium)
+        }
+    }
+}
--- app/src/main/java/com/example/pruebamarvel/presentation/view/MainScreen.kt
+++ app/src/main/java/com/example/pruebamarvel/presentation/view/MainScreen.kt
@@ -10,78 +10,101 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import com.example.pruebamarvel.R
 import com.example.pruebamarvel.presentation.viewmodel.MainViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
     val data by viewModel.data.collectAsState()
+    var (sortedData, setSortedData) = rememberSaveable { mutableStateOf(data) }
+    var expanded by remember { mutableStateOf(false) }
+
+    LaunchedEffect(data) {
+        sortedData = data // Actualizar sortedData cuando los datos cambien
+    }
 
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { Text("Character List") }
+                title = { Text("Marvel Characters") },
+                actions = {
+                    SortMenu(onSortChange = { sortOption ->
+                        setSortedData(
+                            when (sortOption) {
+                                SortOption.NAME -> data.sortedBy { it.name }
+                                SortOption.ID -> data.sortedBy { it.id }
+                            }
+                        )
+                    })
+                }
             )
         },
-        content = {
-            LazyColumn {
-                items(data.size) { index ->
-                    val item = data[index]
-                    ListItem(
-                        imageUrl = item.thumbnail.fullPath,
-                        title = item.name,
-                        subtitle = item.description,
-                        onClick = { /* Navigate to detail screen */ }
-                    )
-                }
-            }
+        content = { paddingValues ->
+            CharacterList(
+                data = sortedData,
+                modifier = Modifier.padding(paddingValues)
+            )
         }
     )
 }
 
 @Composable
-fun ListItem(imageUrl: String, title: String, subtitle: String, onClick: () -> Unit) {
-    Row(modifier = Modifier
-        .fillMaxWidth()
-        .padding(16.dp)
-        .clickable(onClick = onClick)
-    ) {
-        Image(
-            painter = rememberImagePainter(imageUrl),
-            contentDescription = null,
-            modifier = Modifier.size(64.dp),
-            contentScale = ContentScale.Crop
-        )
-        Spacer(modifier = Modifier.width(16.dp))
-        Column {
-            Text(text = title, style = MaterialTheme.typography.titleMedium)
-            Text(text = subtitle, style = MaterialTheme.typography.titleSmall)
-        }
+fun SortMenu(onSortChange: (SortOption) -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(onClick = { expanded = true }) {
+        Icon(Icons.Default.MoreVert, contentDescription = "Sort Options")
     }
-}
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = { expanded = false }
+    ) {
+        DropdownMenuItem(
+            onClick = {
+                onSortChange(SortOption.NAME)
+                expanded = false
+            },
+            text = {
+                Text("Sort by Name")
+            })
 
-@Preview(showBackground = true)
-@Composable
-fun PreviewListItem() {
-    ListItem(
-        imageUrl = "https://via.placeholder.com/150",
-        title = "Character Name",
-        subtitle = "Character Description",
-        onClick = {}
-    )
+        DropdownMenuItem(
+            onClick = {
+                onSortChange(SortOption.ID)
+                expanded = false
+            },
+            text = {
+                Text("Sort by ID")
+            })
+    }
 }
