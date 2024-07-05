3eb5773be52f607acc726e8ee81ae203a2dae4f4
David Aparicio <apadavid@gmail.com>
2024-07-05 13:08:51 +0200

    update: colors and theme modified

--- app/src/main/java/com/example/pruebamarvel/ui/theme/Theme.kt
+++ app/src/main/java/com/example/pruebamarvel/ui/theme/Theme.kt
@@ -21,37 +21,33 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
+)
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorPalette = darkColorScheme(
+    primary = Purple200,
+    primaryContainer = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = Purple500,
+    primaryContainer = Purple700,
+    secondary = Teal200
 )
 
 @Composable
 fun PruebaMarvelTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
         content = content
     )
