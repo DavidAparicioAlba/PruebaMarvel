6b6cf869051ea02de4f369c1000186f884716e77
David Aparicio <apadavid@gmail.com>
2024-07-05 17:50:09 +0200

    remove: code cleaned

--- app/src/main/java/com/example/pruebamarvel/MainActivity.kt
+++ app/src/main/java/com/example/pruebamarvel/MainActivity.kt
@@ -3,14 +3,6 @@ package com.example.pruebamarvel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.pruebamarvel.presentation.view.MainScreen
 import com.example.pruebamarvel.ui.theme.PruebaMarvelTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,27 +11,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             PruebaMarvelTheme {
                 MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PruebaMarvelTheme {
-        Greeting("Android")
     }
 }
