94aa2fea5ccfe91b9d54b4d52fd19fc80877532a
David Aparicio <apadavid@gmail.com>
2024-07-05 12:02:45 +0200

    delete: import deleted

--- app/src/main/java/com/example/pruebamarvel/presentation/view/MainScreen.kt
+++ app/src/main/java/com/example/pruebamarvel/presentation/view/MainScreen.kt
@@ -3,7 +3,6 @@ package com.example.pruebamarvel.presentation.view
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.pruebamarvel.presentation.viewmodel.MainViewModel
