53c835ca10b8809cd77b4b8db14b0a1b35d04ee4
David Aparicio <apadavid@gmail.com>
2024-07-08 10:48:58 +0200

    add: webview navigation added

--- app/src/main/java/com/example/pruebamarvel/domain/model/Url.kt
+++ app/src/main/java/com/example/pruebamarvel/domain/model/Url.kt
@@ -3,4 +3,7 @@ package com.example.pruebamarvel.domain.model
 data class Url(
     val type: String,
     val url: String
-)
+) {
+    val getUrl: String
+        get() = url.replace("http:", "https:")
+}
--- app/src/main/java/com/example/pruebamarvel/presentation/view/CharacterDetailScreen.kt
+++ app/src/main/java/com/example/pruebamarvel/presentation/view/CharacterDetailScreen.kt
@@ -1,34 +1,63 @@
 package com.example.pruebamarvel.presentation.view
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
-import com.example.pruebamarvel.domain.model.HeroModel
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import com.example.pruebamarvel.presentation.viewmodel.MainViewModel
 
 @Composable
-fun CharacterDetailScreen(character: HeroModel) {
-    Column(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Image(
-            painter = rememberImagePainter(character.thumbnail.fullPath),
-            contentDescription = null,
+fun CharacterDetailScreen(viewModel: MainViewModel = hiltViewModel(), characterId: Int, navController: NavController) {
+
+    val state by viewModel.data.collectAsState()
+    val character = state.find { it.id == characterId }
+
+    character?.let {
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = character.name, style = MaterialTheme.typography.titleMedium)
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = character.description, style = MaterialTheme.typography.bodyMedium)
+                .fillMaxSize()
+                .padding(top = 20.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            val imageUrl = character.thumbnail.fullPath
+            Image(
+                painter = rememberAsyncImagePainter(imageUrl),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = character.name, style = MaterialTheme.typography.headlineMedium)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = character.description, style = MaterialTheme.typography.bodyLarge)
+            Spacer(modifier = Modifier.height(16.dp))
+
+            character.urls.forEach { url ->
+                Text(
+                    text = url.type,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.clickable {
+                        navController.navigate("webview?url=${url.getUrl}")
+                    }
+                )
+            }
+        }
     }
 }
