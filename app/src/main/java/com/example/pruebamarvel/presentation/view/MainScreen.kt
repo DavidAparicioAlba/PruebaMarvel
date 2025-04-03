93bc6ddb8245a75ad240188749bcbef4241c750b
David Aparicio <apadavid@gmail.com>
2025-04-03 02:50:14 +0200

    bug displaying data fixed, redundant variables removed, sortData added in MainViewModel

--- app/src/main/java/com/example/pruebamarvel/presentation/view/MainScreen.kt
+++ app/src/main/java/com/example/pruebamarvel/presentation/view/MainScreen.kt
@@ -14,12 +14,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,15 +28,9 @@ import com.example.pruebamarvel.presentation.viewmodel.MainViewModel
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel = hiltViewModel(), navController: NavController) {
-
-    val data by viewModel.data.collectAsState()
+    val sortedData by viewModel.sortedData.collectAsState()
     val isLoading by viewModel.isLoading.collectAsState()
     val errorMessage by viewModel.errorMessage.collectAsState()
-    var (sortedData, setSortedData) = rememberSaveable { mutableStateOf(data) }
-
-    LaunchedEffect(data) {
-        sortedData = data
-    }
 
     Scaffold(
         topBar = {
@@ -46,12 +38,7 @@ fun MainScreen(viewModel: MainViewModel = hiltViewModel(), navController: NavCon
                 title = { Text("Marvel Characters") },
                 actions = {
                     SortMenu(onSortChange = { sortOption ->
-                        setSortedData(
-                            when (sortOption) {
-                                SortOption.NAME -> data.sortedBy { it.name }
-                                SortOption.ID -> data.sortedBy { it.id }
-                            }
-                        )
+                        viewModel.sortData(sortOption)
                     })
                 }
             )
--- app/src/main/java/com/example/pruebamarvel/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/pruebamarvel/presentation/viewmodel/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pruebamarvel.domain.model.HeroModel
 import com.example.pruebamarvel.domain.usecase.GetDataUseCase
+import com.example.pruebamarvel.presentation.view.SortOption
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -18,6 +19,9 @@ class MainViewModel @Inject constructor(
     private val _data = MutableStateFlow<List<HeroModel>>(emptyList())
     val data: StateFlow<List<HeroModel>> get() = _data
 
+    private val _sortedData = MutableStateFlow<List<HeroModel>>(emptyList())
+    val sortedData: StateFlow<List<HeroModel>> = _sortedData
+
     private val _isLoading = MutableStateFlow(false)
     val isLoading: StateFlow<Boolean> = _isLoading
 
@@ -30,15 +34,24 @@ class MainViewModel @Inject constructor(
 
     private fun fetchData() {
         _isLoading.value = true
+        _errorMessage.value = null
         viewModelScope.launch {
             try {
                 val result = getDataUseCase.execute()
                 _data.value = result
+                _sortedData.value = result
             } catch (e: Exception) {
                 _errorMessage.value = e.message
             } finally {
                 _isLoading.value = false
             }
+        }
+    }
+
+    fun sortData(sortOption: SortOption) {
+        _sortedData.value = when (sortOption) {
+            SortOption.NAME -> _data.value.sortedBy { it.name }
+            SortOption.ID -> _data.value.sortedBy { it.id }
         }
     }
 }
